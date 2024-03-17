3a27344978a3aa404c2c74b42671d2b3db4ea04d
amrit kuikel <amritkuikel5689@example.com>
2024-03-17 19:39:27 +0545

    final dev hub commit in new repo

---
+++ app/api/openai/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+export const POST = async (request: Request) => {
+  try {
+    const { question } = await request.json();
+    const genAI = new GoogleGenerativeAI(process.env.API_KEY!);
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    const prompt = question;
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    return NextResponse.json({ text });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message });
+  }
+};
