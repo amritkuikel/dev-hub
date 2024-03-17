3b5aa22aa7c3b3cccde716420a0cb039027f74d8
amrit kuikel <amritkuikel5689@example.com>
2024-03-17 21:49:42 +0545

    db change

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -13,7 +13,7 @@ export const connectToDatabase = async () => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
-      dbName: "DevHub",
+      dbName: "DevOverflow",
     });
 
     isConnected = true;
